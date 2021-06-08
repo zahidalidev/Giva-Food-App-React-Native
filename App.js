9a4f25f6b632ef2ab0ffc9034826d0daa75f7be8
zahidalidev <m.zahidalidev@gmail.com>
2021-06-08 17:06:47 +0500

    fromated

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, LogBox, ImagePropTypes } from 'react-native';
+import { RFPercentage } from "react-native-responsive-fontsize"
+import AnimatedSplash from "react-native-animated-splash-screen";
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { NavigationContainer } from "@react-navigation/native"
+
+import LoginScreen from "./app/screeens/LoginScreen"
+import RegisterScreen from "./app/screeens/RegisterScreen"
+import HomeScreen from "./app/screeens/HomeScreen"
+
+import colors from "./app/config/colors"
+
+const Stack = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='login' drawerType="front" overlayColor="transparent" edgeWidth={100} drawerStyle={{
+        backgroundColor: colors.white,
+        width: 0
+      }}
+      >
+        <Stack.Screen name="loginScreen" >{(props) => <LoginScreen {...props} />}</Stack.Screen>
+        <Stack.Screen name="registerScreen" >{(props) => <RegisterScreen {...props} />}</Stack.Screen>
+        <Stack.Screen name="homeScreen" >{(props) => <HomeScreen {...props} />}</Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ app/config/colors.js
@@ -0,0 +1,13 @@
+export default {
+    primary: "#ffc244",
+    secondary: "#008169",
+    black: "#000",
+    white: "#fff",
+    medium: "#6e6969",
+    light: "#f8f4f4",
+    dark: "#0c0c0c",
+    danger: "#ff5252",
+    lightGrey: "#efefef",
+    mediumGrey: "#b2b2b2",
+    grey: "#808080"
+}
