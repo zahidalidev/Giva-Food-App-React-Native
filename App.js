3acfca6b57e280d371fad7dfb062cdb1698c538a
zahidalidev <m.zahidalidev@gmail.com>
2021-06-11 18:52:08 +0500

    drawer

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, LogBox, ImagePropTypes } from 'react-native';
 import { RFPercentage } from "react-native-responsive-fontsize"
 import AnimatedSplash from "react-native-animated-splash-screen";
-// import { createDrawerNavigator } from "@react-navigation/drawer"
+import { createDrawerNavigator } from "@react-navigation/drawer"
 import { createStackNavigator } from "@react-navigation/stack"
 import { NavigationContainer } from "@react-navigation/native"
 
@@ -14,10 +14,13 @@ import HomeScreen from "./app/screeens/HomeScreen"
 import ProductScreen from "./app/screeens/ProductScreen"
 import ProductDetailsScreen from "./app/screeens/ProductDetailsScreen"
 import CartScreen from "./app/screeens/CartScreen"
+import ProfileScreen from "./app/screeens/ProfileScreen"
+import OrdersScreen from "./app/screeens/OrdersScreen"
 
 import colors from "./app/config/colors"
+import AppDrawer from './app/components/AppDrawer';
 
-// const Stack = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -33,16 +36,18 @@ export default function App() {
     },
   };
 
-  return (
-    <NavigationContainer>
+  function Root() {
+    return (
       <Stack.Navigator screenOptions={{
         headerShown: false,
         transitionSpec: {
           open: config,
           close: config,
         },
-      }} initialRouteName='cartScreen' drawerType="front" overlayColor="transparent" edgeWidth={100}
+      }} initialRouteName='loginScreen'
       >
+        <Stack.Screen name="ordersScreen" >{(props) => <OrdersScreen {...props} />}</Stack.Screen>
+        <Stack.Screen name="profileScreen" >{(props) => <ProfileScreen {...props} />}</Stack.Screen>
         <Stack.Screen name="loginScreen" >{(props) => <LoginScreen {...props} />}</Stack.Screen>
         <Stack.Screen name="registerScreen" >{(props) => <RegisterScreen {...props} />}</Stack.Screen>
         <Stack.Screen name="homeScreen" >{(props) => <HomeScreen {...props} />}</Stack.Screen>
@@ -50,6 +55,22 @@ export default function App() {
         <Stack.Screen name="productDetailsScreen" >{(props) => <ProductDetailsScreen {...props} />}</Stack.Screen>
         <Stack.Screen name="cartScreen" >{(props) => <CartScreen {...props} />}</Stack.Screen>
       </Stack.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="root" drawerType={"front"}
+        overlayColor="transparent"
+        edgeWidth={100}
+        drawerStyle={{
+          backgroundColor: colors.white,
+          width: "75%"
+        }}
+        drawerContent={(props) => <AppDrawer {...props} />}
+      >
+        <Drawer.Screen name="root" component={Root} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
